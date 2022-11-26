56149dc2d77781eb241a9f0b4c2b1d27f907ea3b
saulius <info@kurokesu.com>
2022-11-26 08:59:37 +0200

    test 2nd app

---
+++ second_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+if "celsius" not in st.session_state:
+    # set the initial default value of the slider widget
+    st.session_state.celsius = 50.0
+
+st.slider(
+    "Temperature in Celsius",
+    min_value=-100.0,
+    max_value=100.0,
+    key="celsius"
+)
+
+# This will get the value of the slider widget
+st.write(st.session_state.celsius)
